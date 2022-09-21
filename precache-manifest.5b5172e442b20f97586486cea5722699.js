ee07f46087ee41749393d28fac20176522d34ac4
olamide owolabi <olamz2003'gmail.com>
2022-09-21 19:05:48 +0100

    Updates

---
+++ precache-manifest.5b5172e442b20f97586486cea5722699.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9169f5cf017e5185305",
+    "url": "/High-Life-Project/static/js/main.e9169f5c.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/High-Life-Project/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "60b3443da21823f6415c",
+    "url": "/High-Life-Project/static/js/runtime~main.60b3443d.js"
+  },
+  {
+    "revision": "7ef092eccccb7b8461939b839e5d85fe",
+    "url": "/High-Life-Project/index.html"
+  }
+];
